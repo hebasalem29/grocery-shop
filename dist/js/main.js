338deb2635ff36950c4765dc09029cb9c7e09b76
hebasalem29 <hebanassr58@gmail.com>
2021-02-11 11:12:32 +0200

    faq is done elhamdullah

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,22 @@
 $(function()
 {
+    "use strict";
 
+$(".faq1").on('click',function()
+{
+   
+    $(this).siblings(".faq1").children(".fa").removeClass("dis");
+    $(this).siblings('.faq1').children(".ime").removeClass("fa-minus");
+    $(this).siblings('.faq1').children(".ime").addClass("fa-plus");
+    $(this).children(".ime").toggleClass("fa-plus").toggleClass("fa-minus");
+    $(this).children('.fa').toggleClass("dis");
+    
+   
+    
+    
+
+    console.log("hi");
+
+
+});
 });
